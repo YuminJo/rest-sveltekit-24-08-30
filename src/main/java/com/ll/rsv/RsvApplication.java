ddeb0e84dc913777866113b8a24030f2f17dd7cf
조유민 <nationmilo@gmail.com>
2024-08-30 23:17:54 +0900

    init: Initalize Settings

---
+++ src/main/java/com/ll/rsv/RsvApplication.java
@@ -0,0 +1,13 @@
+package com.ll.rsv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RsvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RsvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ll/rsv/RsvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.rsv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RsvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
