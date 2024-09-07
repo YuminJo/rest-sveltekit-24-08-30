51e1aaba38c0eaa17710e356bd5a50525d791ba5
조유민 <nationmilo@gmail.com>
2024-09-07 22:20:17 +0900

    feat: init layout settings

--- front/src/routes/+page.svelte
+++ front/src/routes/+page.svelte
@@ -1,6 +1,2 @@
-<script>
-	import '$lib/app.css';
-</script>
-
 <h1 class="text-red-500">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
