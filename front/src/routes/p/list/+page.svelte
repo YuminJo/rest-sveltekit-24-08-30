def4c088db00579f2d60c070b6ca08bf8a339d35
조유민 <nationmilo@gmail.com>
2024-09-20 23:46:53 +0900

    feat: env address

--- front/src/routes/p/list/+page.svelte
+++ front/src/routes/p/list/+page.svelte
@@ -2,7 +2,7 @@
 	import createClient from 'openapi-fetch';
 	import type { paths } from '$lib/types/api/v1/schema';
 
-	const { GET } = createClient<paths>({ baseUrl: 'http://localhost:8090/' });
+	const { GET } = createClient<paths>({ baseUrl: import.meta.env.VITE_API_URL });
 
 	async function load() {
 		const { data } = await GET('/api/v1/posts', {});
