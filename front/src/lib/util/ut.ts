a5bd35b5e95eee5600ee28024e249c48f392e643
조유민 <nationmilo@gmail.com>
2024-10-08 10:23:14 +0900

    feat: my profile

---
+++ front/src/lib/util/ut.ts
@@ -0,0 +1,10 @@
+class Ut {
+    public prettyDate(date: string) {
+      return date.substring(2, 10);
+    }
+    public prettyDateTime(date: string) {
+      return date.substring(2, 16).replace('T', ' ');
+    }
+  }
+  const ut = new Ut();
+  export default ut;
---
+++ front/src/routes/member/me/+page.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import rq from '$lib/rq/rq.svelte';
+	import ut from '$lib/util/ut';
+</script>
+
+<h1>내 정보</h1>
+<div>
+	<div>
+		<div>
+			<img src={rq.member.profileImgUrl} width="100" class="rounded" alt="" />
+		</div>
+	</div>
+	<div>
+		<div>번호</div>
+		<div>{rq.member.id}</div>
+	</div>
+	<div>
+		<div>가입날짜</div>
+		<div>{ut.prettyDate(rq.member.createDate)}</div>
+	</div>
+	<div>
+		<div>이름</div>
+		<div>{rq.member.name}</div>
+	</div>
+</div>
